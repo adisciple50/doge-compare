9826f781105840dcf23bf5c81ec0e6cff3b2e1cc
Ben Cullen <Holographic.sol@gmail.com>
2022-05-27 19:33:20 +0100

    beta is working

--- arbitrage/database.py
+++ arbitrage/database.py
@@ -6,16 +6,16 @@ class Db:
         self.con = sqlite3.connect(database)  # database
 
     def create_database_if_not_exists(self):
-        self.con.execute("CREATE TABLE IF NOT EXISTS main.quotes (coin_to_gbp float NOT NULL,coin_to_doge_exchange_rate float NOT NULL,exchanged_doge_worth_in_gbp float NOT NULL,gbp_difference float NOT NULL);")
+        self.con.execute("CREATE TABLE IF NOT EXISTS main.quotes (coin TEXT NOT NULL,coin_to_gbp float NOT NULL,coin_to_doge_exchange_rate float NOT NULL,exchanged_doge_worth_in_gbp float NOT NULL,profit_in_gbp float NOT NULL);")
         self.con.commit()
 
     # coin is the coin to convert to doge and
-    def insert_quote(self,coin_to_gbp:float,coin_to_doge_exchange_rate:float,exchanged_doge_worth_in_gbp:float,gbp_difference:float):
-        self.con.execute("INSERT INTO quotes(coin_to_gbp,coin_to_doge_exchange_rate,exchanged_doge_worth_in_gbp,gbp_difference) VALUES(?,?,?,?)",(coin_to_gbp,coin_to_doge_exchange_rate,exchanged_doge_worth_in_gbp,gbp_difference))
+    def insert_quote(self, coin:str, coin_to_gbp:float, coin_to_doge_exchange_rate:float, exchanged_doge_worth_in_gbp:float, profit_in_gbp:float):
+        self.con.execute("INSERT INTO quotes(coin,coin_to_gbp,coin_to_doge_exchange_rate,exchanged_doge_worth_in_gbp,profit_in_gbp) VALUES(?,?,?,?,?)", (coin, coin_to_gbp, coin_to_doge_exchange_rate, exchanged_doge_worth_in_gbp, profit_in_gbp))
         self.con.commit()
 
     def sort_by_gbp_difference(self):
-        self.con.execute("SELECT * FROM quotes ORDER BY gbp_difference")
+        self.con.execute("SELECT * FROM quotes ORDER BY profit_in_gbp ASC")
         self.con.commit()
 
     def truncate_table(self):
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         doge = miner.convert_to_doge(coin)
         coin_rate = miner.to_gbp(coin)
         doge_worth = miner.doge_to_gbp_quote(doge)
-        difference = float(coin_rate) - float(doge_worth)
-        db.insert_quote(coin_rate, doge, doge_worth, difference)
+        profit = float(doge_worth) - float(coin_rate)
+        db.insert_quote(coin, coin_rate, doge, doge_worth, profit)
     print(db.sort_by_gbp_difference())
 # doge is not the amount bought by the pair currency
