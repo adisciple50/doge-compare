4a22039a1dd12c345c9efc31197ea8196f41dd7f
Ben Cullen <Holographic.sol@gmail.com>
2022-05-27 01:20:30 +0100

    alpha

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from arbitrage.database import Db
-from arbitrage.miner import Miner
-
-miner = Miner()
-db = Db("miner.db")
-
 if __name__ == '__main__':
+    from arbitrage.database import Db
+    from arbitrage.miner import Miner
+
+    miner = Miner()
+    db = Db("miner.db")
+    db.truncate_table()
     currencies = miner.get_all_coins()
-    for coin in currencies():
+    for coin in currencies:
         doge = miner.convert_to_doge(coin)
         coin_rate = miner.to_gbp(coin)
         doge_worth = miner.doge_to_gbp_quote(doge)
         difference = doge_worth - coin_rate
         db.insert_quote(coin_rate, doge, doge_worth, difference)
-
-print(db.sort_by_gbp_difference())
+    print(db.sort_by_gbp_difference())
